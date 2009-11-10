cc5cf1c631c4ad5f980e1d4b48e96bbce113d528
petter <petter@0463f305-d864-43cb-8a47-61cf597d4139>
2009-11-10 14:53:05 +0000

    more practical bash start script
    git-svn-id: https://www2.in.tum.de/repos/ttt/trunk@66 0463f305-d864-43cb-8a47-61cf597d4139

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+PRG=`type $0`
+PRG=${PRG##* }
+# If PRG is a symlink, trace it to the real home directory
+while [ -L "$PRG" ]
+do
+    newprg=$(ls -l ${PRG})
+    newprg=${newprg##*-> }
+    [ ${newprg} = ${newprg#/} ] && newprg=${PRG%/*}/${newprg}
+    PRG="$newprg"
+done
+PRG=${PRG%/*}
+echo Changing to application folder is ${PRG}
+cd ${PRG}
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
     echo Executing 64 bit version
@@ -7,4 +20,4 @@ else
     echo Executing 32 bit version
     LD_LIBRARY_PATH=linux32 java -Xmx1024M -jar ttt.jar
 fi
-
+cd -
