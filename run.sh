c583f49f30d386a13abc1e0fe1f0a19a521d754b
petter <petter@0463f305-d864-43cb-8a47-61cf597d4139>
2011-01-27 13:31:27 +0000

    Swing-Darstellung mit aktivierter OpenGL-Unterstuetzung
    git-svn-id: https://www2.in.tum.de/repos/ttt/trunk@87 0463f305-d864-43cb-8a47-61cf597d4139

--- run.sh
+++ run.sh
@@ -15,9 +15,9 @@ cd ${PRG}
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
     echo Executing 64 bit version
-    LD_LIBRARY_PATH=linux64 java -Xmx1024M -jar ttt.jar
+    LD_LIBRARY_PATH=linux64 java -Dsun.java2d.opengl=true -Xmx1024M -jar ttt.jar
 else
     echo Executing 32 bit version
-    LD_LIBRARY_PATH=linux32 java -Xmx1024M -jar ttt.jar
+    LD_LIBRARY_PATH=linux32 java -Dsun.java2d.opengl=true -Xmx1024M -jar ttt.jar
 fi
 cd -
