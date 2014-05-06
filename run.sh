459f381b27228ffdf82db3ed05b071196919ce3b
petter <petter@0463f305-d864-43cb-8a47-61cf597d4139>
2014-05-06 13:18:30 +0000

    more RAM
    git-svn-id: https://www2.in.tum.de/repos/ttt/trunk@165 0463f305-d864-43cb-8a47-61cf597d4139

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ cd ${PRG}
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
     echo Executing 64 bit version
-    LD_LIBRARY_PATH=linux64 java -Dsun.java2d.opengl=true -Xmx1024M -jar ttt.jar
+    LD_LIBRARY_PATH=linux64 java -Dsun.java2d.opengl=true -Xmx4096M -jar ttt.jar
 else
     echo Executing 32 bit version
     LD_LIBRARY_PATH=linux32 java -Dsun.java2d.opengl=true -Xmx1024M -jar ttt.jar
