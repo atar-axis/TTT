698296f05372076e07f3b2b88a5d62689ca4c2d3
Dr. Michael Petter <petter@in.tum.de>
2016-06-14 13:15:29 +0200

    updated scripts for 16:9 devices and new touch handling

---
+++ beamer169.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xrandr --output eDP1 --mode 1360x720
+xrandr --output eDP1 --mode 1360x720 --pos 0x0 --output HDMI2 --mode 1280x720 --pos 0x0
---
+++ vncserver169.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+vncserver -kill :1
+vncserver -geometry 1280x720
+command -v kdialog || {
+    command -v zenity || { echo 'VNC started'; exit 1 ; }
+    zenity --info --text="VNC started"
+    exit 1
+}
+kdialog -msgbox 'VNC started'
